d70a18ae735492915f41e9504ff65a4d403beddb
Leland Weeks <lhw22@drexel.edu>
2026-03-14 12:00:37 -0700

    changed the data structure of the layers to be able to better support multiple conv layers

--- src/model.py
+++ src/model.py
@@ -9,42 +9,53 @@ class MinecraftCNN(nn.Module):
         POOL_KERNEL_SIZE = 2
         POOL_STRIDE = 2 
 
+        # image size: 320x180 pixels
+        # 3 color channels (RGB) or 1 channel for grayscale
+        # no alpha channel
+        img_width = 320
+        img_height = 180    
+
         # compute dimensions for the linear layer
-        # image files input: 320x180 pixels, 3 color channels (RGB), no alpha channel
-        conv_width  = (320 - kernel_size) / CONV_STRIDE + 1
-        conv_height = (180 - kernel_size) / CONV_STRIDE + 1
+        conv_width  = (img_width - kernel_size) / CONV_STRIDE + 1
+        conv_height = (img_height - kernel_size) / CONV_STRIDE + 1
         pool_width  = (conv_width  - POOL_KERNEL_SIZE) / POOL_STRIDE + 1
         pool_height = (conv_height - POOL_KERNEL_SIZE) / POOL_STRIDE + 1
         linear_input = int(num_kernels * pool_width * pool_height)
 
-        # buildl the 'base' model
-        self.model = nn.Sequential(
+        # define the layers
+        layers = []
+        for i in range(num_conv_layers):
             # output width = (input_width - kernel_size + 2*padding) / stride + 1
             # output height = (input_height - kernel_size + 2*padding) / stride + 1
             # calculate width: (320 - 3 + 2*0) / 1 + 1 = 318
             # calculate height: (180 - 3 + 2*0) / 1 + 1 = 178
-            nn.Conv2d(in_channels=in_channels, out_channels=num_kernels, kernel_size=kernel_size, stride=CONV_STRIDE, padding=0),
+            layers.append(nn.Conv2d(in_channels=in_channels, out_channels=num_kernels, kernel_size=kernel_size, stride=CONV_STRIDE, padding=0))
             
             # input is an image so we want all values to be non-negative
             # also, ReLU is default activation layer for CNNs
-            nn.ReLU(),
-            
-            # reduce dimensions to most important features, reducing computational cost
-            # output width = (input_width - kernel_size) / stride + 1
-            # output height = (input_height - kernel_size) / stride + 1
-            # calculate width: (318 - 2) / 2 + 1 = 159
-            # calculate height: (178 - 2) / 2 + 1 = 89
-            nn.MaxPool2d(kernel_size=POOL_KERNEL_SIZE, stride=POOL_STRIDE), 
-            
-            # flatten the image into a vector so linear layer can determine the class scores for each image
-            # 159x89 -> 1x14,191
-            nn.Flatten(),
+            layers.append(nn.ReLU())
 
-            # fully connected layer to output class scores
-            # input size = num_kernels * height * width
-            # calculation: 1 * 159 * 89 = 14,191
-            nn.Linear(linear_input, num_classes) 
-        )
+
+        # reduce dimensions to most important features, reducing computational cost
+        # output width = (input_width - kernel_size) / stride + 1
+        # output height = (input_height - kernel_size) / stride + 1
+        # calculate width: (318 - 2) / 2 + 1 = 159
+        # calculate height: (178 - 2) / 2 + 1 = 89
+        layers.append(nn.MaxPool2d(kernel_size=POOL_KERNEL_SIZE, stride=POOL_STRIDE))
+
+        # flatten the image into a vector so linear layer can determine the class scores for each image
+        # calculation: 159x89 -> 1x14,191 for 1 conv layer
+        layers.append(nn.Flatten())
+
+        # fully connected layer to output class scores
+        # input size = num_kernels * height * width
+        # calculation: 1 * 159 * 89 = 14,191 for 1 conv layer
+        layers.append(nn.Linear(linear_input, num_classes))
+
+        # build the model
+        self.model = nn.Sequential(*layers)
+
+
 
     def forward(self, x):
         return self.model(x)
